663f9c872468a132ca3416cb902316e23ffc1205
Dasaba03 <david.sanchez@live.com.mx>
2026-04-29 20:34:21 -0600

    Identation regex patterns

--- src/constants/regex/indentación.ts
+++ src/constants/regex/indentación.ts
@@ -2,11 +2,9 @@
  * Combined indentation regex patterns for easier access
  */
 
-
-//provisionales les ahce falta cambio por que ni idea de como hacerlo gracias 
-export const NEWLINE = /\n/;
-export const INDENT = /[ ]{4}/; // Assuming 4 spaces for indentation
-export const DEDENT = /(?<![ ]{4})/;
+export const NEWLINE = /\r?\n/;
+export const INDENT = /^[ \t]+/;
+export const DEDENT = /(?<![\s\S])/;
 
 export const indentation = {
   NEWLINE,
